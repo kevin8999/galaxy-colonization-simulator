3a402823676a2e42cdb8b47201a77a2bbdb73775
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-07-17 09:44:21 -0400

    feat: add Calculator::calcDistance()
    Calculates the distance between two points in n dimensions (provided Star A and Star B have the same lengths)

--- simulation/Calculator.cpp
+++ simulation/Calculator.cpp
@@ -1,1 +1,22 @@
 #include "Calculator.h"
+
+#include <cmath>
+#include <iostream>
+
+float Calculator::calcDistance(Star& s1, Star& s2) {
+    const std::vector<float> pos1 = s1.position;
+    const std::vector<float> pos2 = s2.position;
+
+    if (pos1.size() != pos2.size()) {
+        std::cerr << "ERROR: pos1 != pos2 " << "(" << pos1.size() << " != " << pos2.size() << ")" << std::endl;
+    }
+
+    float dist = 0.0f;
+
+    for (int val = 0; val < pos1.size(); ++val) {
+        const float displacement = pos2[val] - pos1[val];
+        dist += std::pow(displacement, 2);
+    }
+
+    return std::sqrt(dist);
+}
