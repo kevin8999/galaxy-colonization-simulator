38f5c698affadf64a0b84b661b0acf2e0d9839ba
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-07-17 08:46:18 -0400

    chore: move Star class out of include/ and src/ folders
    This commit moves the header and source file into the same parent directory.

--- simulation/Star.h
+++ simulation/Star.h
@@ -7,11 +7,14 @@ struct Star {
     std::vector<int> position;
 
     float solarLuminosity;
-    int id;
+    unsigned int id;
+    bool visited;
 
-    Star(std::vector<int> inPos, float inLum, inID) {
+    Star(std::vector<int> inPos, float inLum, unsigned int inID) {
         position = inPos;
         solarLuminosity = inLum;
         id = inID;
     }
+
+    std::vector<Star> findClosestStars(unsigned int n);  // Finds closest n stars
 }
