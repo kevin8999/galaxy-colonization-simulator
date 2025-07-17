63018f188f31b7efeb9977efa01abe2e1bf86d7f
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-07-17 08:48:19 -0400

    feat: Add Calculator class

---
+++ simulation/Calculator.h
@@ -0,0 +1,8 @@
+#include "Star.h"
+
+class Calculator {
+    // Performs calculations on the Galaxy and Star classes
+
+    // Calculates distance between star 1 and star 2
+    float calcDistance(Star& s1, Star& s2);
+}
