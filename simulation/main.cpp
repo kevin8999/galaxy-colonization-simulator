060ddff610513c272e4fd4f6f93632004bb34186
Kevin D <76125913+Vomet@users.noreply.github.com>
2025-07-28 15:21:17 -0400

    chore: Add test for KDTree in main.cpp

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -2,11 +2,25 @@
 #include "Star.h"
 #include "Calculator.h"
 
+#include "KDTree.h"
+
+#include <iostream>
+
 int main() {
     std::string galaxyFile = "../data/hygdata_cleaned.csv";
     Galaxy galaxy;
     galaxy.load(galaxyFile);
 
+    unsigned int dimensions = 3;
+    KDTree tree(dimensions);
+
+    std::vector<Star> stars = galaxy.getStars();
+    tree.insertStars(stars);
+
+    unsigned int max = 30;
+    tree.print(max);
+
+    std::cout << "Finished" << std::endl;
 
     return 0;
 }
