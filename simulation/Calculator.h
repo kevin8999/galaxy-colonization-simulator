804ec208d47743726fb7a5898605c11a8b549f7e
Kevin D <kevinduong306@gmail.com>
2025-07-30 00:01:46 -0400

    fix: Add header files for `Calculator::calcDistance()` and make it `static`
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Calculator.h
+++ simulation/Calculator.h
@@ -1,8 +1,11 @@
 #include "Star.h"
+#include "Node.h"
 
 class Calculator {
     // Performs calculations on the Galaxy and Star classes
 
-    // Calculates distance between star 1 and star 2
-    float calcDistance(Star& s1, Star& s2);
+    public:
+        // Calculates distance between star 1 and star 2
+        static float calcDistance(Star& s1, Star& s2);
+        static float calcDistance(Node* n1, Node* n2);
 };
