6350d789d37b71741e1f354948630a44454b61de
Kevin D <kevinduong306@gmail.com>
2025-07-30 00:00:00 -0400

    chore: Minor performance upgrades to `Calculator::calcDistance()`
    MAIN CHANGES
    
    - Change `pos1` and `pos2` to be reference variables
    - Change `std::pow(displacement, 2)` to `displacement * displacement`
    for readability
    
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Calculator.cpp
+++ simulation/Calculator.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 
 float Calculator::calcDistance(Star& s1, Star& s2) {
-    const std::vector<float> pos1 = s1.position;
-    const std::vector<float> pos2 = s2.position;
+    const std::vector<float>& pos1 = s1.position;
+    const std::vector<float>& pos2 = s2.position;
 
     if (pos1.size() != pos2.size()) {
         std::cerr << "ERROR: pos1 != pos2 " << "(" << pos1.size() << " != " << pos2.size() << ")" << std::endl;
@@ -15,7 +15,7 @@ float Calculator::calcDistance(Star& s1, Star& s2) {
 
     for (int val = 0; val < pos1.size(); ++val) {
         const float displacement = pos2[val] - pos1[val];
-        dist += std::pow(displacement, 2);
+        dist += displacement * displacement;
     }
 
     return std::sqrt(dist);
