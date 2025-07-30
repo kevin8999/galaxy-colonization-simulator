da3b08d978e9a1234083f1c85c151d071319663f
Kevin D <kevinduong306@gmail.com>
2025-07-30 00:00:44 -0400

    feat: Add `Calculator::calcDistance()`
    `Calculator::calcDistance()` calculates the distance between two `Node`
    objects
    
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Calculator.cpp
+++ simulation/Calculator.cpp
@@ -20,3 +20,21 @@ float Calculator::calcDistance(Star& s1, Star& s2) {
 
     return std::sqrt(dist);
 }
+
+float Calculator::calcDistance(Node* s1, Node* s2) {
+    const std::vector<float>& pos1 = s1->position;
+    const std::vector<float>& pos2 = s2->position;
+
+    if (pos1.size() != pos2.size()) {
+        std::cerr << "ERROR: pos1 != pos2 " << "(" << pos1.size() << " != " << pos2.size() << ")" << std::endl;
+    }
+
+    float dist = 0.0f;
+
+    for (int val = 0; val < pos1.size(); ++val) {
+        const float displacement = pos2[val] - pos1[val];
+        dist += displacement * displacement;
+    }
+
+    return std::sqrt(dist);
+}
