08d7152169b9862fcfcd376d0f0e929d0bca5b58
Kevin D <kevinduong306@gmail.com>
2025-07-30 13:46:06 -0400

    fix: Throw compiler errors if function encounters invalid parameters
    This commit forces `Calculator::calcDistance()` to throw an error if:
    
    - Any input `Node*` objects are `nullptr`
    - If `s1->position.size() != s2->position.size()`. If the dimensions of
    the positions are different, their distance cannot be calculated
    
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Calculator.cpp
+++ simulation/Calculator.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <stdexcept>
 
 float Calculator::calcDistance(Star& s1, Star& s2) {
     const std::vector<float>& pos1 = s1.position;
@@ -22,11 +23,15 @@ float Calculator::calcDistance(Star& s1, Star& s2) {
 }
 
 float Calculator::calcDistance(Node* s1, Node* s2) {
+    if (s1 == nullptr || s2 == nullptr) {
+        throw std::invalid_argument("Calculator::calcDistance() : One or both nodes are nullptr.");
+    }
+
     const std::vector<float>& pos1 = s1->position;
     const std::vector<float>& pos2 = s2->position;
 
     if (pos1.size() != pos2.size()) {
-        std::cerr << "ERROR: pos1 != pos2 " << "(" << pos1.size() << " != " << pos2.size() << ")" << std::endl;
+        throw std::invalid_argument("Calculator::calcDistance() : Dimension mismatch between position vectors.");
     }
 
     float dist = 0.0f;
