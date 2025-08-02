0b7d19059261788a7bedc7c74a068f9825307ce0
Kevin D <kevinduong306@gmail.com>
2025-08-02 16:15:17 -0400

    feat: Allow `Calculator::calcDistance()` to accept two `Star*` arguments
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Calculator.cpp
+++ simulation/Calculator.cpp
@@ -22,6 +22,24 @@ float Calculator::calcDistance(Star& s1, Star& s2) {
     return std::sqrt(dist);
 }
 
+float Calculator::calcDistance(Star* s1, Star* s2) {
+    const std::vector<float>& pos1 = s1->position;
+    const std::vector<float>& pos2 = s2->position;
+
+    if (pos1.size() != pos2.size()) {
+        std::cerr << "ERROR: pos1 != pos2 " << "(" << pos1.size() << " != " << pos2.size() << ")" << std::endl;
+    }
+
+    float dist = 0.0f;
+
+    for (int val = 0; val < pos1.size(); ++val) {
+        const float displacement = pos2[val] - pos1[val];
+        dist += displacement * displacement;
+    }
+
+    return std::sqrt(dist);
+}
+
 float Calculator::calcDistance(Node* s1, Node* s2) {
     if (s1 == nullptr || s2 == nullptr) {
         throw std::invalid_argument("Calculator::calcDistance() : One or both nodes are nullptr.");
--- simulation/Calculator.h
+++ simulation/Calculator.h
@@ -4,8 +4,9 @@
 class Calculator {
     // Performs calculations on the Galaxy and Star classes
 
-    public:
-        // Calculates distance between star 1 and star 2
-        static float calcDistance(Star& s1, Star& s2);
-        static float calcDistance(Node* n1, Node* n2);
+public:
+    // Calculates distance between star 1 and star 2
+    static float calcDistance(Star& s1, Star& s2);
+    static float calcDistance(Star *s1, Star *s2);
+    static float calcDistance(Node* n1, Node* n2);
 };
