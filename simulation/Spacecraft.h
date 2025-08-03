7c002b8cb131a1395299a3c2310bfb498a3cfa0e
Kevin D <kevinduong306@gmail.com>
2025-08-02 21:28:00 -0400

    feat: Add `Spacecraft::launch()` with pre-calculated `distance` param
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

--- simulation/Spacecraft.h
+++ simulation/Spacecraft.h
@@ -60,4 +60,14 @@ struct Spacecraft {
 
         enRoute = true;
     }
+
+    void launch(const float& departTime, const float& distance) {
+        departureTime = departTime;
+
+        totalDistance = distance;
+        this->travelTime = static_cast<float>(velocity / totalDistance);
+        this->arrivalTime = departureTime + travelTime;
+
+        enRoute = true;
+    }
 };
