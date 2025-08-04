cbd902eb65ee5db106f48d52018bd9804aa911e0
Kevin D <kevinduong306@gmail.com>
2025-08-04 03:10:26 -0400

    feat: Add CONSTANTS.h
    CONSTANTS.h contains all the constants needed for this program.
    
    Signed-off-by: Kevin D <kevinduong306@gmail.com>

---
+++ simulation/CONSTANTS.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cmath>
+
+// Stores constants for entire program
+
+// Speed of light in meters per second
+const float SPEED_OF_LIGHT = 299'792'458;
+// Light year in meters
+const double LIGHT_YEAR = 9.461 * std::pow(10, 15);
